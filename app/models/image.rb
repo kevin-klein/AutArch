cd1a5545ce8bd07313d8a15ded3e5e6b5c7a4c5e
kevin <kevin.k1252@gmail.com>
2022-11-07 11:46:26 +0100

    update

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :bigint           not null, primary key
+#  data       :binary
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Image < ApplicationRecord
 end
--- app/models/page_image.rb
+++ app/models/page_image.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: page_images
+#
+#  id         :bigint           not null, primary key
+#  page_id    :bigint           not null
+#  image_id   :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class PageImage < ApplicationRecord
   belongs_to :page
   belongs_to :image
