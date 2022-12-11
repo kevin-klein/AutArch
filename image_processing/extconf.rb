8e62eccdb29e0fac7c6a71a2f67342b46f0817db
Kevin <kevin.k1252@gmail.com>
2022-12-12 00:56:16 +0100

    osx compile fix

--- image_processing/extconf.rb
+++ image_processing/extconf.rb
@@ -1,5 +1,6 @@
 require 'mkmf'
 
+$CXXFLAGS += " -stdlib=libc++ -std=c++11"
 $CXXFLAGS += " #{`pkg-config --cflags --libs opencv4`}"
 # $LDFLAGS += " -ltesseract "
 $LDFLAGS += " #{`pkg-config --cflags --libs opencv4`}"
