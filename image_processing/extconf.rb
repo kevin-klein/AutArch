b32b51be708e5264c16675aa22a662b40b7b6eb9
Kevin <kevin.k1252@gmail.com>
2023-03-14 02:40:35 +0100

    darwin fixes

--- image_processing/extconf.rb
+++ image_processing/extconf.rb
@@ -2,9 +2,9 @@ require 'mkmf'
 
 if Gem::Platform.local.os === 'darwin'
 $CXXFLAGS += ' -stdlib=libc++ -std=c++17'
-else  
-  $CXXFLAGS += " #{`pkg-config --cflags --libs opencv4`}"
-  $LDFLAGS += " #{`pkg-config --cflags --libs opencv4`}"
+else 
 end
+$CXXFLAGS += " #{`pkg-config --cflags --libs opencv4`}"
+$LDFLAGS += " #{`pkg-config --cflags --libs opencv4`}"
 
 create_makefile('ext')
