5349bfa516c14896b424783dad8292864dbc9411
kevin <kevin.k1252@gmail.com>
2023-06-04 17:12:11 +0200

    cleanup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,6 @@ import * as mdb from 'mdb-ui-kit';
 
 import 'chartkick/chart.js';
 import bootstrap from 'bootstrap';
-import StimulusReflex from 'stimulus_reflex';
-import consumer from '../channels/consumer';
-import controller from '../controllers';
 
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
