b532879cfdc90084324aa847beddf4610960b17b
kevin <kevin.k1252@gmail.com>
2023-06-04 16:54:30 +0200

    deploy stuff

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'popper';
 import 'bootstrap';
-import './controllers';
-import './channels/test_channel';
