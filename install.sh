91714044337a365f3e1052d1c9e11ba193a41582
kevin <kevin.k1252@gmail.com>
2023-06-04 16:54:30 +0200

    deploy stuff

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo apt install -y libgsl-dev libopencv-dev libvips-dev gnupg2 libjemalloc-dev
+
+gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
+source ~/.bashrc
+
+rvm install 3.1.2 -C -with-jemalloc
+
+sudo snap install inkscape
+bundle
+
+wget https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh
+bash install.sh
+source ~/.bashrc
+nvm install v18
+npm install -g yarn
+yarn
+
+cd image_processing
+ruby extconf.rb
+make
