ff385c291e02a4a607ca4921b74e3653c971b2d1
Kevin <kevin.k1252@gmail.com>
2023-11-27 21:13:04 +0100

    chart colors

--- app/lib/pca.rb
+++ app/lib/pca.rb
@@ -1,15 +1,32 @@
 class Pca
   attr_reader :explained_variance_ratio
-  def initialize opts = {}
-    @pca = PCA.new(n_components: 2)
+  def initialize(scale_data: false, components: 2, whiten: true)
+    @pca = PCA.new(n_components: components)
+    @scale_data = scale_data
+    # @scale_data = false
   end
 
   def fit(x)
+    if @scale_data
+      create_sd(x)
+      x = scale(x)
+    end
+
     @pca.fit(x)
     @explained_variance_ratio = @pca.explained_variance_ratio_
   end
 
   def transform(x)
+    x = scale(x) if @scale_data
     @pca.transform(x)
+  end
+
+  def create_sd(data)
+    @scaler = StandardScaler.new(with_mean: false)
+    @scaler.fit(data)
+  end
+
+  def scale(data)
+    @scaler.transform(data)
   end
 end
