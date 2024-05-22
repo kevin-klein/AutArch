845c139f1c7de09753186f2816906da834296a94
Kevin <kevin.k1252@gmail.com>
2024-05-22 12:04:52 +0200

    ui updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,27 @@
 // import loadingAttributePolyfill from "loading-attribute-polyfill/dist/loading-attribute-polyfill.module.js";
 // import * as mdb from 'mdb-ui-kit';
 
-import 'chartkick/chart.js';
-import bootstrap from 'bootstrap';
+import React from 'react'
+import 'chartkick/chart.js'
+import bootstrap from 'bootstrap'
 
-import BoxResizer from '../components/BoxResizer';
-import AddFigures from '../components/AddFigures';
-import NorthArrow from '../components/NorthArrow';
+import BoxResizer from '../components/BoxResizer'
+import AddFigures from '../components/AddFigures'
+import NorthArrow from '../components/NorthArrow'
+import EuropeMap from '../components/EuropeMap'
 
-import ReactOnRails from 'react-on-rails';
-ReactOnRails.register({ BoxResizer, AddFigures, NorthArrow });
+import ReactOnRails from 'react-on-rails'
+import { Filler, LineElement, PointElement, RadialLinearScale, Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 
-import Rails from '@rails/ujs';
-Rails.start();
+import Rails from '@rails/ujs'
+
+ChartJS.register(Filler, LineElement, PointElement, ArcElement, Tooltip, Legend, RadialLinearScale)
+ReactOnRails.register({
+  BoxResizer,
+  AddFigures,
+  NorthArrow,
+  EuropeMap: function (props, railsContext) {
+    return () => <EuropeMap {...props} />
+  }
+})
+Rails.start()
---
+++ app/models/bone_tool.rb
@@ -0,0 +1,55 @@
+# == Schema Information
+#
+# Table name: figures
+#
+#  id                  :bigint           not null, primary key
+#  page_id             :bigint           not null
+#  x1                  :integer          not null
+#  x2                  :integer          not null
+#  y1                  :integer          not null
+#  y2                  :integer          not null
+#  type                :string           not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  area                :float
+#  perimeter           :float
+#  meter_ratio         :float
+#  angle               :float
+#  parent_id           :integer
+#  identifier          :string
+#  width               :float
+#  height              :float
+#  text                :string
+#  site_id             :bigint
+#  validated           :boolean          default(FALSE), not null
+#  verified            :boolean          default(FALSE), not null
+#  disturbed           :boolean          default(FALSE), not null
+#  contour             :text             default([]), not null
+#  deposition_type     :integer          default(0), not null
+#  publication_id      :integer
+#  percentage_scale    :integer
+#  page_size           :integer
+#  manual_bounding_box :boolean          default(FALSE)
+#  bounding_box_angle  :integer
+#  control_point_1_x   :integer
+#  control_point_1_y   :integer
+#  control_point_2_x   :integer
+#  control_point_2_y   :integer
+#  control_point_3_x   :integer
+#  control_point_3_y   :integer
+#  control_point_4_x   :integer
+#  control_point_4_y   :integer
+#  anchor_point_1_x    :integer
+#  anchor_point_1_y    :integer
+#  anchor_point_2_x    :integer
+#  anchor_point_2_y    :integer
+#  anchor_point_3_x    :integer
+#  anchor_point_3_y    :integer
+#  anchor_point_4_x    :integer
+#  anchor_point_4_y    :integer
+#  probability         :float
+#
+class BoneTool < Figure
+  belongs_to :grave, foreign_key: 'parent_id', optional: true, inverse_of: :spines
+
+end
