e2ad1d5d58ec999bf2da377af4329db7bd1761cf
Kevin <kevin.k1252@gmail.com>
2024-06-16 21:43:18 +0200

    fixes

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -175,7 +175,7 @@ class PublicationsController < AuthorizedController
   def set_compare_graves
     @other_publications = params
       .dig(:compare, :publication_id)
-      &.take(3)
+      &.take(4)
       &.map { Publication.includes(:figures).find(_1) if _1.present? }
       &.compact
   end
