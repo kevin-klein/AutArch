e8acd06ecce85753966700e48d556dc4a1499355
Kevin <kevin.k1252@gmail.com>
2024-06-17 02:59:22 +0200

    fixed charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,61 @@ import NorthArrow from '../components/NorthArrow'
 import EuropeMap from '../components/EuropeMap'
 import ImportProgress from '../components/ImportProgress'
 import SiteMap from '../components/SiteMap'
+import Chart from "react-apexcharts";
+
+function ScatterChart({ data, colors }) {
+  console.log(data)
+  console.log(colors)
+
+  const series = data.map(item => ({
+    ...item,
+    name: `${item.name} (N = ${item.data.length})`,
+    data: item.data.map(dataItem => [dataItem.x, dataItem.y])
+  }))
+
+  return <Chart type="scatter" series={series} options={{
+    tooltip: {
+      custom: function({series, seriesIndex, dataPointIndex, w}) {
+        return '<div class="arrow_box" style="padding: 10;">' +
+          '<span>Grave ' + data[seriesIndex].data[dataPointIndex].title + '</span>' +
+          '</div>'
+      }
+    },
+    colors: colors,
+    legend: {
+      fontSize: 16
+    },
+    chart: {
+      animations: {
+        enabled: false
+      },
+      events: {
+        markerClick: function(event, chartContext, { seriesIndex, dataPointIndex, config}) {
+          const item = data[seriesIndex].data[dataPointIndex]
+          const link = `/graves/${item.id}/update_grave/set_grave_data`
+          window.location.href = link
+        }
+      },
+    },
+    xaxis: {
+      type: 'numeric',
+      tickAmount: 10,
+      labels: {
+        formatter: function(val) {
+          return parseFloat(val).toFixed(1)
+        }
+      }
+    },
+    yaxis: {
+      tickAmount: 8,
+      labels: {
+        formatter: function(val) {
+          return parseFloat(val).toFixed(1)
+        }
+      }
+    },
+  }} />
+}
 
 import ReactOnRails from 'react-on-rails'
 import { Filler, LineElement, PointElement, RadialLinearScale, Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
@@ -22,6 +77,7 @@ ReactOnRails.register({
   BoxResizer,
   ImportProgress,
   AddFigures,
+  ScatterChart,
   NorthArrow,
   SiteMap: function (props, railsContext) {
     return () => <SiteMap {...props} />
