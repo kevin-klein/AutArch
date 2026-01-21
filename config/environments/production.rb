1c05c25ab8acfede59fb526ec51670057372ec51
Kevin <kevin.k1252@gmail.com>
2026-01-21 14:05:07 +0100

    master key fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.secret_key_base = ENV['RAILS_MASTER_KEY']
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
