e5f68925d3cc57c1532e566a51f09a40791aa9ec
Kevin <kevin.k1252@gmail.com>
2026-01-22 11:08:19 +0100

    cleanup

--- app/lib/analyze_skeleton.rb
+++ app/lib/analyze_skeleton.rb
@@ -1,29 +1,29 @@
 class AnalyzeSkeleton
-  @@model = Vision::CenterNet::PoseModel.new(
-    backbone: 'resnet101',
-    input_size: 256,
-    heatmap_size: 64
-  )
-  @@model.load_state_dict(Torch.load("models/center_net_keypoints.pth"))
-  @@model.eval
+  # @@model = Vision::CenterNet::PoseModel.new(
+  #   backbone: 'resnet101',
+  #   input_size: 256,
+  #   heatmap_size: 64
+  # )
+  # @@model.load_state_dict(Torch.load("models/center_net_keypoints.pth"))
+  # @@model.eval
 
   def run(skeleton)
-    transforms = TorchVision::Transforms::Compose.new([
-      TorchVision::Transforms::Resize.new([256, 256]),
-      TorchVision::Transforms::ToTensor.new
-    ])
+  #   transforms = TorchVision::Transforms::Compose.new([
+  #     TorchVision::Transforms::Resize.new([256, 256]),
+  #     TorchVision::Transforms::ToTensor.new
+  #   ])
 
-    image_data = MinOpenCV.imencode(
-      MinOpenCV.extractFigure(skeleton, skeleton.page.image.data)
-    )
-    image = Vips::Image.new_from_buffer(image_data, "")
-    image = image.colourspace("srgb")
-    input = transforms.call(image)
+  #   image_data = MinOpenCV.imencode(
+  #     MinOpenCV.extractFigure(skeleton, skeleton.page.image.data)
+  #   )
+  #   image = Vips::Image.new_from_buffer(image_data, "")
+  #   image = image.colourspace("srgb")
+  #   input = transforms.call(image)
 
-    result = @@model.predict_keypoints(Torch.stack([input]), threshold: 0.3)
+  #   result = @@model.predict_keypoints(Torch.stack([input]), threshold: 0.3)
 
-    raise
+  #   raise
 
-    @@model.format_predictions(result, image.width, image.height)
+  #   @@model.format_predictions(result, image.width, image.height)
   end
 end
