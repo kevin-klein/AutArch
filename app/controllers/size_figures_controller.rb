def2c7432c824f66db6d78c147681501d6f64020
Kevin <kevin.k1252@gmail.com>
2026-01-29 14:44:30 +0100

    sam fix

--- app/controllers/size_figures_controller.rb
+++ app/controllers/size_figures_controller.rb
@@ -51,6 +51,6 @@ class SizeFiguresController < ApplicationController
   end
 
   def set_figure
-    @figure = @figure_cls.find(params[:id])
+    @figure = @figure_cls.find(params[:size_figure_id])
   end
 end
