c6485cfed5ef8ad58e5f15cee0feb4dfa03dd059
Kevin <kevin.k1252@gmail.com>
2026-04-28 01:17:32 +0200

    figures preview login fix

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,5 @@
 class FiguresController < AuthorizedController
+  skip_before_action :preview, only: [:preview]
   before_action :set_figure, only: %i[show edit preview update destroy]
 
   # GET /figures or /figures.json
